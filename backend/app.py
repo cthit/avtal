2a60275bdda16f37fb2e62aa163300d99a11ea7a
hall <carl.j.holmberg@telia.com>
2019-10-18 18:59:18 +0200

    read all markdown files

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, Response
+import json
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
 
@@ -8,5 +11,24 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/api', methods=['GET'])
+def get_all_agreements():
+    agreement_path = "./agreements/"
+    files = [f for f in listdir(agreement_path) if isfile(join(agreement_path, f))]
+    json_objects = []
+    for file in files:
+        s = open(agreement_path + file, 'r')
+        x = {
+            "name": file.split('.')[0],
+            "agreement": s.read(),
+            "accepted": True
+        }
+        json_objects.append(x)
+
+    return Response(json.dumps(json_objects), mimetype='application/json')
+
+
 if __name__ == '__main__':
     app.run()
+
+# Supply and store markdown files.
