d1b3c50bf4c2803a2b78f16ecd7b53bc07d27f98
hall <carl.j.holmberg@telia.com>
2019-11-28 18:22:28 +0100

    Working requests

--- frontend/src/use-cases/avtal/avtal.jsx
+++ frontend/src/use-cases/avtal/avtal.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { putAccept, getAgreement } from "../../api/avtal/put.accept.api";
+import { putAccept } from "../../api/avtal/put.accept.api";
+import { getAgreement } from "../../api/avtal/get.agreement.api";
+import { getAccept } from "../../api/avtal/get.accept.api";
 import { DigitMarkdown, DigitButton } from "@cthit/react-digit-components";
 import { useParams } from "react-router";
 
@@ -19,14 +21,33 @@ const AvtalButton = styled.div`
 const Avtal = () => {
     //Read from api
     const { service } = useParams();
-    const md = getAgreement(service);
+    const [md, setMd] = useState("null");
+
+    getAgreement(service)
+        .then(res => {
+            setMd(res.data);
+        })
+        .catch(error => {
+            console.log("error wioo wioo:", error);
+        });
+
+    getAccept(service)
+        .then(res => {
+            console.log(res.data);
+            if (res.data) {
+                //redirectToSource("https://chalmers.it");
+            }
+        })
+        .catch(error => {
+            console.log("error: arbitrary");
+        });
 
     return (
         <AvtalHolder>
             <DigitMarkdown markdownSource={md} />
             <AvtalButton>
                 <DigitButton
-                    //Change to translated string
+                    //Change to translated stringimezone.utc)
                     text="Accept the Agreement"
                     primary
                     raised
@@ -39,8 +60,17 @@ const Avtal = () => {
 
 function agreementAccepted(serviceName) {
     // TODO: GET SERIVCE NAME FROM ENDPOINT
-    putAccept(serviceName);
-    console.log(serviceName);
+    putAccept(serviceName)
+        .then(res => {
+            redirectToSource("https://google.com");
+        })
+        .catch(error => {
+            console.log("hello darkness my old friend");
+        });
+}
+
+function redirectToSource(source) {
+    window.location.replace(source);
 }
 
 export default Avtal;
